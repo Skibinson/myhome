dd66415f23939cf3db608b99f8c1884b5f0a3bd9
Skibinson <anton.skibin32131@gmail.com>
2026-03-20 14:17:10 +0300

    tool(server): add sensor-simulaation

--- server/tools/simulate_sensor.py
+++ server/tools/simulate_sensor.py
@@ -1,10 +1,20 @@
 import requests
 import time 
-
+import random
 URL = "http://localhost:8000/api/reading"
 DEVICE_ID = "kitchen_01"
 
-data = {
+while True:
+    data = {
         "device_id" : DEVICE_ID,
-
+        "readings" :
+        {
+        "temperature" : random.uniform(-30, 30),
+        "motion" : random.choice([True, False]),
+        "leak" : random.choice([True, False])
         }
+        }
+
+    response = requests.post(URL, json=data)
+    print(f"Sent: {data}, Response: {response.status_code}")
+    time.sleep(5)
